af08f1371c803f81608a85895d96fffe2229e5be
vishal <vishalkoolkarni0045@gmail.com>
2024-04-27 16:33:13 +0530

    feat: completed phase0 assigning value

--- src/pk_encryption_circuit.rs
+++ src/pk_encryption_circuit.rs
@@ -1,6 +1,14 @@
-use axiom_eth::rlc::circuit::RlcCircuitParams;
-use halo2_base::gates::circuit::BaseCircuitParams;
+use axiom_eth::rlc::{
+    chip::RlcChip,
+    circuit::{builder::RlcCircuitBuilder, instructions::RlcCircuitInstructions, RlcCircuitParams},
+};
+use halo2_base::{
+    gates::{circuit::BaseCircuitParams, RangeChip},
+    utils::ScalarField,
+};
 use serde::Deserialize;
+
+use crate::poly::{Poly, PolyAssigned};
 
 /// Helper function to define the parameters of the RlcCircuit. This is a non-optimized configuration that makes use of a single advice column. Use this for testing purposes only.
 pub fn test_params() -> RlcCircuitParams {
@@ -37,6 +45,87 @@ pub struct BfvPkEncryptionCircuit {
     k1: Vec<String>,
     r2is: Vec<Vec<String>>,
     r1is: Vec<Vec<String>>,
-    ais: Vec<Vec<String>>,
     ct0is: Vec<Vec<String>>,
+}
+
+/// Payload returned by the first phase of the circuit to be reused in the second phase
+pub struct Payload<F: ScalarField> {
+    pk0_qi_assigned: PolyAssigned<F>,
+    u_assigned: PolyAssigned<F>,
+    e0_assigned: PolyAssigned<F>,
+    k1_assigned: PolyAssigned<F>,
+    r2is_assigned: Vec<PolyAssigned<F>>,
+    r1is_assigned: Vec<PolyAssigned<F>>,
+    ct0is: Vec<Vec<String>>,
+}
+
+impl<F: ScalarField> RlcCircuitInstructions<F> for BfvPkEncryptionCircuit {
+    type FirstPhasePayload = Payload<F>;
+
+    /// #### Phase 0
+    /// TODO: Figure out whether to assign the pk0_qi in phase0 or not , verifieer knows the pk(doubt)
+
+    /// In this phase, the polynomials for each matrix $S_i$ are assigned to the circuit. Namely:
+    /// * polynomials `u`, `e0`, `k1`, `pk0_qi` are assigned to the witness table. This has to be done only once as these polynomial are common to each $S_i$ matrix
+    /// * polynomials `r1i`, `r2i` are assigned to the witness table for each $S_i$ matrix
+
+    /// Witness values are element of the finite field $\mod{p}$. Negative coefficients $-z$ are assigned as field elements $p - z$.
+
+    /// At the end of phase 0, the witness generated so far is interpolated into a polynomial and committed by the prover. The hash of this commitment is used as challenge and will be used as a source of randomness $\gamma$ in Phase 1. This feature is made available by Halo2 [Challenge API](https://hackmd.io/@axiom/SJw3p-qX3).
+
+    fn virtual_assign_phase0(
+        &self,
+        builder: &mut RlcCircuitBuilder<F>,
+        _: &RangeChip<F>,
+    ) -> Self::FirstPhasePayload {
+        let ctx = builder.base.main(0);
+
+        let pk0_qi = Poly::<F>::new(self.pk0_qi.clone());
+        let pk0_qi_assigned = PolyAssigned::new(ctx, pk0_qi);
+
+        let u = Poly::<F>::new(self.u.clone());
+        let u_assigned = PolyAssigned::new(ctx, u);
+
+        let e0 = Poly::<F>::new(self.e0.clone());
+        let e0_assigned = PolyAssigned::new(ctx, e0);
+
+        let k1 = Poly::<F>::new(self.k1.clone());
+        let k1_assigned = PolyAssigned::new(ctx, k1);
+
+        let r1is_assigned = self
+            .r1is
+            .iter()
+            .map(|r1is| {
+                let r1is = Poly::<F>::new(r1is.clone());
+                PolyAssigned::new(ctx, r1is)
+            })
+            .collect::<Vec<_>>();
+
+        let r2is_assigned = self
+            .r2is
+            .iter()
+            .map(|r2is| {
+                let r2is = Poly::<F>::new(r2is.clone());
+                PolyAssigned::new(ctx, r2is)
+            })
+            .collect::<Vec<_>>();
+
+        Payload {
+            pk0_qi_assigned,
+            u_assigned,
+            e0_assigned,
+            k1_assigned,
+            r2is_assigned,
+            r1is_assigned,
+            ct0is: self.ct0is.clone(),
+        }
+    }
+
+    fn virtual_assign_phase1(
+        builder: &mut RlcCircuitBuilder<F>,
+        range: &RangeChip<F>,
+        rlc: &RlcChip<F>,
+        payload: Self::FirstPhasePayload,
+    ) {
+    }
 }
